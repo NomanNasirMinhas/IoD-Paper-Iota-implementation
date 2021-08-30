ebd7ceca832d42ade524a40a18454db76c050454
Noman Nasir Minhas <sp17bsse010@gmail.com>
2021-08-30 11:07:31 +0500

    Base Paper Added

--- src/drone.js
+++
@@ -1,8 +0,0 @@
-const sha256 = require("js-sha256").sha256;
-
-function getDroneCode(droneID, code){
-  let droneCode = code + droneID;
-  return sha256(droneCode.toString())
-}
-
-module.exports = {getDroneCode}
